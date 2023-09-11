738e30315738918fd7246179de4b22d94068322f
badass-techie <moses.odhiambo.555@gmail.com>
2023-09-11 10:05:03 +0300

    Fix urls

--- src/urls.ts
+++ src/urls.ts
@@ -1,8 +1,8 @@
 export class Urls {
-    private static readonly utuApiBase = `https://stage-api.ututrust.com/core-api-v2`;
+    private static readonly utuApiBase = `https://stage-api.ututrust.com`;
     public static readonly auth = `${Urls.utuApiBase}/identity-api/verify-address`;
-    public static readonly entity = `${Urls.utuApiBase}/entity`;
-    public static readonly feedback = `${Urls.utuApiBase}/feedback`;
-    public static readonly feedbackSummary = `${Urls.utuApiBase}/feedbackSummary`;
+    public static readonly entity = `${Urls.utuApiBase}/core-api-v2/entity`;
+    public static readonly feedback = `${Urls.utuApiBase}/core-api-v2/feedback`;
+    public static readonly feedbackSummary = `${Urls.utuApiBase}/core-api-v2/feedbackSummary`;
     public static readonly networkUrl = `https://rpc-mumbai.maticvigil.com`;
 }
