9f92e35be4cc3c8f15b8317666b0957163367121
Dennis Kimathi <Kimathidennis38@gmail.com>
2023-09-12 11:27:11 +0300

    get ai response

--- src/bot.ts
+++ src/bot.ts
@@ -1,165 +1,212 @@
 import { Bot, Context, InlineKeyboard, session, SessionFlavor } from 'grammy';
+import OpenAI from 'openai';
+import { config } from 'dotenv'; // Import config from dotenv
+
+
+// Load environment variables from .env file
+config();
+
+// Create an instance of the OpenAI API
+
+const openaiApi = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY as string,
+});
+
 
 // Define the states of the bot
 enum State {
-    IDLE,
-    AWAITING_PRIVATE_KEY,
-    AWAITING_USERNAME,
-    AWAITING_ACTION,
-    AWAITING_FEEDBACK,
-    AWAITING_RATING,
-    AWAITING_FEEDBACK_CONFIRMATION,
+  IDLE,
+  AWAITING_PRIVATE_KEY,
+  AWAITING_USERNAME,
+  AWAITING_ACTION,
+  AWAITING_FEEDBACK,
+  AWAITING_RATING,
+  AWAITING_FEEDBACK_CONFIRMATION,
 }
 
 interface SessionData {
-    state: State;   // the current state of the bot
-    otherUsername: string;  // the username of the user to get or submit feedback on
-    feedback: string;   // the feedback to submit
-    rating: number; // the rating to submit
+  state: State; // the current state of the bot
+  otherUsername: string; // the username of the user to get or submit feedback on
+  feedback: string; // the feedback to submit
+  rating: number; // the rating to submit
 }
 
 // Declare `ctx.session` to be of type `SessionData`.
 type MyContext = Context & SessionFlavor<SessionData>;
 
 // Create a bot
-const bot = new Bot<MyContext>(process.env.BOT_TOKEN!);
+const bot = new Bot<MyContext>(process.env.BOT_TOKEN as string);
 
 // Install session middleware, and define the initial session value.
 function initial(): SessionData {
-    return {
-        state: State.IDLE,
-        otherUsername: '',
-        feedback: '',
-        rating: 0,
-    };
+  return {
+    state: State.IDLE,
+    otherUsername: '',
+    feedback: '',
+    rating: 0,
+  };
 }
 bot.use(session({ initial }));
 
 // /start command
-bot.command("start", async (ctx) => {
-    // TODO: Should we generate our bot's responses with a language model to make them more expressive and different each time? 🤔
-    await ctx.reply('<b>Web3 Guardian 🤖</b>\n\nA telegram bot that leverages the UTU Web3 Protocol to provide reliable reputation checks on telegram users 🧐', {parse_mode: 'HTML'});
-    // TODO: Include instructions on how to obtain a private key from a wallet such as Metamask in the message below
-    await ctx.reply('Please enter your wallet\'s private key 🔐:');   
-    ctx.session.state = State.AWAITING_PRIVATE_KEY;
+bot.command('start', async (ctx) => {
+  // TODO: Should we generate our bot's responses with a language model to make them more expressive and different each time? 🤔
+
+  const prompt =
+    'Web3 Guardian 🤖\n\nA telegram bot that leverages the UTU Web3 Protocol to provide reliable reputation checks on telegram users 🧐';
+ 
+console.log("prompt",prompt)
+	const response =  await openaiApi.completions.create({
+    model: 'text-davinci-003',
+    prompt: prompt,
+    max_tokens: 50,
+    temperature: 0.7,
+    n: 1,
+    stop: '\n',
+  });
+
+  const welcomeMessage = response.choices[0].text.trim();
+
+  await ctx.reply(welcomeMessage, { parse_mode: 'HTML' });
+  // TODO: Include instructions on how to obtain a private key from a wallet such as Metamask in the message below
+  await ctx.reply("Please enter your wallet's private key 🔐:");
+  ctx.session.state = State.AWAITING_PRIVATE_KEY;
 });
 
 // /restart command
-bot.command("restart", async (ctx) => {
-    if (ctx.session.state !== State.AWAITING_PRIVATE_KEY) {
-        ctx.session = initial();
-        ctx.session.state = State.AWAITING_USERNAME;
-        await ctx.reply('Enter a user\'s username 👤:');
-    }
+bot.command('restart', async (ctx) => {
+  if (ctx.session.state !== State.AWAITING_PRIVATE_KEY) {
+    ctx.session = initial();
+    ctx.session.state = State.AWAITING_USERNAME;
+    await ctx.reply("Enter a user's username 👤:");
+  }
 });
 
 // Middleware to handle user inputs
 bot.on(['message:text', 'callback_query:data'], async (ctx) => {
-    if (ctx.session.state === State.AWAITING_PRIVATE_KEY) {
-        const privateKey = ctx.message?.text || ctx.callbackQuery?.data;
-        const myUsername = ctx.from?.username;
-        // TODO: Obtain JWT from UTU API using the private key
-        // TODO: Store the JWT in Redis with the hash as the user's username and the key as 'accessToken'
-        // TODO: Store the refresh token (if any) in Redis with the hash as the user's username and the key as 'refreshToken'
-        // Now, ask the user to enter a user's username
-        await ctx.reply('Enter a user\'s username 👤:');
-        ctx.session.state = State.AWAITING_USERNAME;
-    }
-    else if (ctx.session.state === State.AWAITING_USERNAME) {
-        ctx.session.otherUsername = ctx.message?.text! || ctx.callbackQuery?.data!;
-        // TODO: check if this username exists in telegram and if not, prompt the user to reenter username
-        ctx.session.state = State.AWAITING_ACTION;
-        await ctx.reply('What would you like to do?', {
-            reply_markup: new InlineKeyboard().text('View User\'s Reputation 👀', 'View User Reputation').row().text('Submit Review on User 📝', 'Submit Review'),
-        });
-    }
-    else if (ctx.session.state === State.AWAITING_ACTION) {
-        const action = ctx.message?.text || ctx.callbackQuery?.data;
-        if (action === 'View User Reputation') {
-            // TODO: Fetch feedback on entity (otherUsername) from UTU API
+  if (ctx.session.state === State.AWAITING_PRIVATE_KEY) {
+    const privateKey = ctx.message?.text || ctx.callbackQuery?.data;
+    const myUsername = ctx.from?.username;
+    // TODO: Obtain JWT from UTU API using the private key
+    // TODO: Store the JWT in Redis with the hash as the user's username and the key as 'accessToken'
+    // TODO: Store the refresh token (if any) in Redis with the hash as the user's username and the key as 'refreshToken'
+    // Now, ask the user to enter a user's username
+    await ctx.reply("Enter a user's username 👤:");
+    ctx.session.state = State.AWAITING_USERNAME;
+  } else if (ctx.session.state === State.AWAITING_USERNAME) {
+    ctx.session.otherUsername = ctx.message?.text! || ctx.callbackQuery?.data!;
+    // TODO: check if this username exists in telegram and if not, prompt the user to reenter username
+    ctx.session.state = State.AWAITING_ACTION;
+    await ctx.reply('What would you like to do?', {
+      reply_markup: new InlineKeyboard()
+        .text("View User's Reputation 👀", 'View User Reputation')
+        .row()
+        .text('Submit Review on User 📝', 'Submit Review'),
+    });
+  } else if (ctx.session.state === State.AWAITING_ACTION) {
+    const action = ctx.message?.text || ctx.callbackQuery?.data;
+    if (action === 'View User Reputation') {
+      // TODO: Fetch feedback on entity (otherUsername) from UTU API
 
-            const reviews = [
-                'Review 1: Great user!',
-                'Review 2: Very trustworthy.',
-                'Review 3: Highly recommended.',
-            ];
+      const reviews = [
+        'Review 1: Great user!',
+        'Review 2: Very trustworthy.',
+        'Review 3: Highly recommended.',
+      ];
 
-            // Display reviews to the user
-            if (reviews.length > 0) {
-                await ctx.reply('Here are the reviews for @' + ctx.session.otherUsername + ':');
-                for (const review of reviews) {
-                    await ctx.reply(review);
-                }
-            } else {
-                await ctx.reply('No reviews found for @' + ctx.session.otherUsername);
-            }
+      // Display reviews to the user
+      if (reviews.length > 0) {
+        await ctx.reply(
+          'Here are the reviews for @' + ctx.session.otherUsername + ':'
+        );
+        for (const review of reviews) {
+          await ctx.reply(review);
+        }
+      } else {
+        await ctx.reply('No reviews found for @' + ctx.session.otherUsername);
+      }
 
-            await ctx.reply('Thanks for using Web3 Guardian! 😊\n\nEnter /restart to try another user.');
-            ctx.session.state = State.IDLE;
-        }
-        else if (action === 'Submit Review') {
-            await ctx.reply('Tell us your objective feedback on @' + ctx.session.otherUsername + ':');
-            ctx.session.state = State.AWAITING_FEEDBACK;
-        }
-        else {
-            await ctx.reply('Invalid input. Please enter View User Reputation or Submit Review');
-            return;
-        }
+      await ctx.reply(
+        'Thanks for using Web3 Guardian! 😊\n\nEnter /restart to try another user.'
+      );
+      ctx.session.state = State.IDLE;
+    } else if (action === 'Submit Review') {
+      await ctx.reply(
+        'Tell us your objective feedback on @' + ctx.session.otherUsername + ':'
+      );
+      ctx.session.state = State.AWAITING_FEEDBACK;
+    } else {
+      await ctx.reply(
+        'Invalid input. Please enter View User Reputation or Submit Review'
+      );
+      return;
     }
-    else if (ctx.session.state === State.AWAITING_FEEDBACK) {
-        ctx.session.feedback = ctx.message?.text! || ctx.callbackQuery?.data!;
-        await ctx.reply('How would you rate your experience with @' + ctx.session.otherUsername + '?');
-        ctx.session.state = State.AWAITING_RATING;
+  } else if (ctx.session.state === State.AWAITING_FEEDBACK) {
+    ctx.session.feedback = ctx.message?.text! || ctx.callbackQuery?.data!;
+    await ctx.reply(
+      'How would you rate your experience with @' +
+        ctx.session.otherUsername +
+        '?'
+    );
+    ctx.session.state = State.AWAITING_RATING;
 
-        // Create a menu with star emojis for rating
-        await ctx.reply('Choose a rating:', {
-            reply_markup: new InlineKeyboard().text('⭐', '1').text('⭐⭐', '2').text('⭐⭐⭐', '3').row().text('⭐⭐⭐⭐', '4').text('⭐⭐⭐⭐⭐', '5'),
-        });
+    // Create a menu with star emojis for rating
+    await ctx.reply('Choose a rating:', {
+      reply_markup: new InlineKeyboard()
+        .text('⭐', '1')
+        .text('⭐⭐', '2')
+        .text('⭐⭐⭐', '3')
+        .row()
+        .text('⭐⭐⭐⭐', '4')
+        .text('⭐⭐⭐⭐⭐', '5'),
+    });
+  } else if (ctx.session.state === State.AWAITING_RATING) {
+    ctx.session.rating = (ctx.message?.text ||
+      ctx.callbackQuery?.data) as unknown as number; // Convert the star emoji into a rating value
+    if (ctx.session.rating < 1 || ctx.session.rating > 5) {
+      await ctx.reply('Invalid input. Please enter a number between 1 and 5');
+      return;
     }
-    else if (ctx.session.state === State.AWAITING_RATING) {
-        ctx.session.rating = (ctx.message?.text || ctx.callbackQuery?.data) as unknown as number;    // Convert the star emoji into a rating value
-        if (ctx.session.rating < 1 || ctx.session.rating > 5) {
-            await ctx.reply('Invalid input. Please enter a number between 1 and 5');
-            return;
-        }
-        await ctx.reply('Feedback: ' + ctx.session.feedback);
-        await ctx.reply('Rating: ' + ctx.session.rating);
-        ctx.session.state = State.AWAITING_FEEDBACK_CONFIRMATION;
+    await ctx.reply('Feedback: ' + ctx.session.feedback);
+    await ctx.reply('Rating: ' + ctx.session.rating);
+    ctx.session.state = State.AWAITING_FEEDBACK_CONFIRMATION;
 
-        // Create a menu with yes/no options
-        await ctx.reply('Are you sure you want to submit the following feedback? (Yes/No)', {
-            reply_markup: new InlineKeyboard().text('Yes', 'Yes').text('No', 'No'),
-        });
+    // Create a menu with yes/no options
+    await ctx.reply(
+      'Are you sure you want to submit the following feedback? (Yes/No)',
+      {
+        reply_markup: new InlineKeyboard().text('Yes', 'Yes').text('No', 'No'),
+      }
+    );
+  } else if (ctx.session.state === State.AWAITING_FEEDBACK_CONFIRMATION) {
+    const confirmation = ctx.message?.text || ctx.callbackQuery?.data;
+    if (confirmation === 'Yes') {
+      // TODO: Create entity (whose uuid is in the format of an eth address) if it doesn't exist
+      // TODO: Submit feedback on enitiy to UTU API
+      await ctx.reply('Feedback submitted successfully!');
+    } else if (confirmation === 'No') {
+      await ctx.reply('Feedback submission cancelled.');
+    } else {
+      await ctx.reply('Invalid input. Please enter Yes or No');
+      return;
     }
-    else if (ctx.session.state === State.AWAITING_FEEDBACK_CONFIRMATION) {
-        const confirmation = ctx.message?.text || ctx.callbackQuery?.data;
-        if (confirmation === 'Yes') {
-            // TODO: Create entity (whose uuid is in the format of an eth address) if it doesn't exist
-            // TODO: Submit feedback on enitiy to UTU API
-            await ctx.reply('Feedback submitted successfully!');
-        }
-        else if (confirmation === 'No') {
-            await ctx.reply('Feedback submission cancelled.');
-        }
-        else {
-            await ctx.reply('Invalid input. Please enter Yes or No');
-            return;
-        }
 
-        await ctx.reply('Thanks for using Web3 Guardian! 😊\n\nEnter /restart to try another user.');
-        ctx.session.state = State.IDLE;
-    }
+    await ctx.reply(
+      'Thanks for using Web3 Guardian! 😊\n\nEnter /restart to try another user.'
+    );
+    ctx.session.state = State.IDLE;
+  }
 });
 
 // match any callback query
 bot.callbackQuery(/.*/, async (ctx) => {
-    console.log(ctx.callbackQuery?.data);
+  console.log(ctx.callbackQuery?.data);
 });
 
 // Handle errors
 bot.catch((err) => {
-    console.error('An error occurred:', err);
-    // TODO: Send error message to user
+  console.error('An error occurred:', err);
+  // TODO: Send error message to user
 });
 
 // Start the bot
